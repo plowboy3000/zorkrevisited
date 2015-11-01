1dc26ef6c362f159474071f726719a622bf482b4
plowboy3000 <plowboy@gmail.com>
2015-11-01 18:17:38 -0500

    Initial Code
    First cut at object model

---
+++ NEActionVocabulary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NortheastAdventureGame
+{
+    class NEActionVocabulary
+    {
+        private List<string> actionVocabulary;
+
+        public NEActionVocabulary()
+        {
+            actionVocabulary = new List<string>();
+            createVocabulary();
+        }
+
+        private void createVocabulary()
+        {
+            actionVocabulary.Add("Take");
+            actionVocabulary.Add("Drop");
+            actionVocabulary.Add("Examine");
+            actionVocabulary.Add("Inventory");
+        }
+
+        public bool isAction(string action)
+        {
+            bool tempbool = false;
+            foreach (string str in actionVocabulary)
+            {
+                if (str.ToUpper() == action.ToUpper())
+                {
+                    tempbool = true;
+                }
+            }
+            return tempbool;
+        }
+    }
+}
---
+++ NEMapPointConnector.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NortheastAdventureGame
+{
+    class NEMapPointConnector
+    {
+        private NEMapPoint sourcePoint;
+        private NEMapPoint destinationPoint;
+        private string direction;
+           
+        public NEMapPointConnector(NEMapPoint FromPoint, NEMapPoint ToPoint)
+        {
+            sourcePoint = FromPoint;
+            destinationPoint = ToPoint;
+            direction = calculateDirection();
+        }
+
+        private string calculateDirection()
+        {
+            //return calculated string direction
+            string dir = "";
+            int calcX = sourcePoint.PosX - destinationPoint.PosX;
+            int calcY = sourcePoint.PosY - destinationPoint.PosY;
+            if (calcX >= 1 && calcY >= 1)
+            {
+                dir = "Southwest";
+            } else if (calcX >= 1 && calcY == 0)
+            {
+                dir = "West";
+            } else if (calcX >= 1 && calcY <= -1)
+            {
+                dir = "Northwest";
+            } else if (calcX == 0 && calcY <= -1)
+            {
+                dir = "North";
+            } else if (calcX <= -1 && calcY <= -1)
+            {
+                dir = "Northeast";
+            } else if (calcX <= -1 && calcY == 0)
+            {
+                dir = "East";
+            } else if (calcX <= -1 && calcY >= 1)
+            {
+                dir = "Southeast";
+            } else if (calcX == 0 && calcY >= 1)
+            {
+                dir = "South";
+            }
+
+            return dir;
+        }
+
+        public string getDirection()
+        {
+            return direction;
+        }
+
+        public NEMapPoint getDestinationPoint()
+        {
+            return destinationPoint;
+        }
+    }
+}
